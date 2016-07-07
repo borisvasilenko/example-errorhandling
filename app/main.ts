fba5e57e4ae335d9e969ba2225ac1f246eed7ceb
Boris Vasilenko <vasilenko@dreaktor.com>
2016-07-07 14:00:38 +0300

    enabled stack tracing for asynchronous calls (zone.js/dist/long-stack-trace-zone.js)

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,8 @@ import { bootstrap }            from '@angular/platform-browser-dynamic';
 import { AppComponent }         from './app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
+import 'node_modules/zone.js/dist/long-stack-trace-zone.js';
+
 bootstrap(AppComponent, [
   APP_ROUTER_PROVIDERS
 ])
