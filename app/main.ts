66211736d524581d4ec96ccea50cd9f9a5775b6e
Boris Vasilenko <vasilenko@dreaktor.com>
2016-07-07 13:52:02 +0300

    cleanup

--- app/main.ts
+++ app/main.ts
@@ -3,20 +3,7 @@ import { bootstrap }            from '@angular/platform-browser-dynamic';
 import { AppComponent }         from './app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
-import { errorServiceInjectables } from "./error.service";
-
-import 'node_modules/zone.js/dist/long-stack-trace-zone.js';
-
-import * as ngCore from '@angular/core';
-ngCore.enableProdMode();
-
-function createNgZone() {
-    return new ngCore.NgZone({ enableLongStackTrace: true });
-}
-
 bootstrap(AppComponent, [
-  provide(ngCore.NgZone, {useFactory: createNgZone }),
-  APP_ROUTER_PROVIDERS,
-  errorServiceInjectables
+  APP_ROUTER_PROVIDERS
 ])
 .catch(err => console.error(err));
